40da3f7e8dcbfd1dffbdf2ebc2884e8ec2034cac
superscalar <65932360+superscalar@users.noreply.github.com>
2024-04-12 21:48:13 -0300

    Fix para vercel

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const app = express();
 
-const ejercicio_express = require("../express/con_express.js");
-
-app.get("/express", (req, res) => { res.send( ejercicio_express.getTableData() ); });
-
-app.use("/cliente_servidor", express.static("cliente_servidor"));
 app.use(express.static('public'))
 
+const ejercicio_express = require("../express/con_express.js");
+app.get("/express", (req, res) => { res.send( ejercicio_express.getTableData() ); });
 
-app.listen(3001, () => console.log("Server ready on port 3001."));
+app.use("/dom", express.static('dom'))
+app.use("/cliente_servidor", express.static('cliente_servidor'))
+
+
+app.listen(3001, () => console.log("Server ready on port 3001"));
 
 module.exports = app;
